9f34e7835a7d67cfc7d862519de3bed91ef570b7
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2023-05-10 19:13:23 -0600

    Select categorias y estados

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,25 @@ export class HomeComponent {
 
   categorias = [];
   estados = [];
-  apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY3l6YXJneHd3bWpjcHBza2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjU0MzcsImV4cCI6MTk5ODgwMTQzN30._7r2cvTzNNcWVBkGoCf_OjOjK85QwG1UJSf_FUT_p2E';
-  authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY3l6YXJneHd3bWpjcHBza2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjU0MzcsImV4cCI6MTk5ODgwMTQzN30._7r2cvTzNNcWVBkGoCf_OjOjK85QwG1UJSf_FUT_p2E';
+  
+  constructor(private kera3Service: Kera3ServiceService){
+    this.getCategorias();
+    this.getEstados();
+  }
 
-  constructor(private kera3Service: Kera3ServiceService){}
+  
+
+  getCategorias(){
+    this.kera3Service.getCategorias().subscribe((resp: any)=>{
+      this.categorias = resp;
+    });
+  }
+
+  getEstados(){
+    this.kera3Service.getEstados().subscribe((resp: any)=>{
+      this.estados = resp;
+    });
+  }
 
 
 }
--- src/app/services/kera3-service.service.ts
+++ src/app/services/kera3-service.service.ts
@@ -7,23 +7,32 @@ import { HttpClient } from '@angular/common/http';
 export class Kera3ServiceService {
 
   url = 'https://mocyzargxwwmjcppskkc.supabase.co/rest/v1/';
+  apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY3l6YXJneHd3bWpjcHBza2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjU0MzcsImV4cCI6MTk5ODgwMTQzN30._7r2cvTzNNcWVBkGoCf_OjOjK85QwG1UJSf_FUT_p2E';
+  authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vY3l6YXJneHd3bWpjcHBza2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMjU0MzcsImV4cCI6MTk5ODgwMTQzN30._7r2cvTzNNcWVBkGoCf_OjOjK85QwG1UJSf_FUT_p2E';
 
   constructor(private http: HttpClient) { }
 
-  getCategorias(params: any){
-    const headers = { 'content-type': 'application/json'};
-    const body=JSON.stringify(params);
-    console.log(body);
-
-    return this.http.post(this.url + 'categoria?select=*', body,{headers});
+  getCategorias() {
+    const body = {}; // cuerpo vacío si no se especifica
+    const headers = { 
+      'content-type': 'application/json',
+      'apikey': this.apiKey,
+      'Authorization': this.authorization
+    };
+    const options = { headers: headers };
+    return this.http.get(this.url + 'categoria?select=*', options);
   }
+  
 
-  getEstados(params: any){
-    const headers = { 'content-type': 'application/json'};
-    const body=JSON.stringify(params);
-    console.log(body);
-
-    return this.http.post(this.url + 'estado?select=*', body,{headers});
+  getEstados() {
+    const body = {}; // cuerpo vacío si no se especifica
+    const headers = { 
+      'content-type': 'application/json',
+      'apikey': this.apiKey,
+      'Authorization': this.authorization
+    };
+    const options = { headers: headers };
+    return this.http.get(this.url + 'estado?select=*', options);
   }
 
 }
