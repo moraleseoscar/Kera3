f7e1713b7d21ead9c76ff2f65f8634bb8f360f6d
Oscar Estrada Morales <oestrada.3@gmail.com>
2023-05-19 18:48:32 -0600

    Boton agregar producto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Kera3ServiceService } from '../services/kera3-service.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -26,6 +27,22 @@ export class HomeComponent {
     console.log(this.categoriaValue, this.estadoValue)
   }
   
+  holamundo(){
+    Swal.fire({
+      title: 'Agregar producto',
+      html: `<input type="text" id="codigo" class="swal2-input" placeholder="Código">
+      <input type="text" id="nombre" class="swal2-input" placeholder="Nombre">
+      <input type="text" id= "categoria" class="swal2-input" placeholder="Categoria">
+      <input type="text" id= "unidad" class="swal2-input" placeholder="Unidad">`,
+      confirmButtonText: 'Sign in',
+      focusConfirm: false,
+      preConfirm: () => {
+        
+      }
+    }).then((result) => {
+     
+    })
+  }
 
   getCategorias(){
     this.kera3Service.getCategorias().subscribe((resp: any)=>{
