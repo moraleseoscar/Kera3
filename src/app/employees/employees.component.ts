d29b5eb2a33e146fc16653bdeef725330fe60821
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2023-08-11 11:12:58 -0600

    ADD EMPLOYEE WORKS

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -18,7 +18,7 @@ export class EmployeesComponent {
   estadoValue = '0'
   rols : any = []
   dpts : any = []
-
+  instalaciones:any = []
   constructor(private service: Kera3Service) { }
 
   async ngOnInit() {
@@ -26,6 +26,7 @@ export class EmployeesComponent {
     this.data = this.clients.slice(this.minIndex, this.maxIndex)
     this.rols = await this.service.getRoles()
     this.dpts = await this.service.getDepartments()
+    this.instalaciones = await this.service.getInstalaciones()
     console.log(this.dpts)
   }
 
@@ -76,11 +77,15 @@ export class EmployeesComponent {
   onAddEmployee(){
     var rol = `<option value="" disabled selected>Rol</option>`
     var dpt = `<option value="" disabled selected>Departamento</option>`
+    var inst = `<option value="" disabled selected>Instalacion</option>`
     for (let index = 0; index < this.rols.length; index++) {
       rol += `<option value="${this.rols[index].codigo_rol}">${this.rols[index].nombre_rol}</option>`
     }
     for (let index = 0; index < this.dpts.length; index++) {
       dpt += `<option value="${this.dpts[index].codigo_dptm}">${this.dpts[index].nombre_dptm}</option>`
+    }
+    for (let index = 0; index < this.instalaciones.length; index++) {
+      inst += `<option value="${this.instalaciones[index].codigo_instalacion}">${this.instalaciones[index].nombre_instalacion}</option>`
     }
     Swal.fire({
       title: 'Registrar nuevo empleado',
@@ -94,12 +99,14 @@ export class EmployeesComponent {
       <select class="uk-select" id="dpt" placeholder="Categoria">
       ${dpt}
       </select>
+      <select class="uk-select" id="inst" placeholder="Categoria">
+      ${inst}
+      </select>
       <input type="text" id="telefono" class="swal2-input" placeholder="Telefono">
       <input type="email" id="correo" class="swal2-input" placeholder="Correo">
-      <input type="password" id="pwrd1" class="swal2-input" placeholder="Contraseña">
-      <input type="password" id="pwrd2" class="swal2-input" placeholder="Confirma Contraseña">
       `,
       confirmButtonText: 'Agregar',
+      cancelButtonText: 'Cancelar', // Texto del botón Cancelar
       focusConfirm: false,
       preConfirm: () => {
 
@@ -109,24 +116,18 @@ export class EmployeesComponent {
         const apellido = (<HTMLInputElement>document.getElementById('apellido')).value;
         const categoria = (<HTMLInputElement>document.getElementById('categoria')).value;
         const dpt = (<HTMLInputElement>document.getElementById('dpt')).value;
+        const isnt = (<HTMLInputElement>document.getElementById('inst')).value;
         const telefono = (<HTMLInputElement>document.getElementById('telefono')).value;
         const correo = (<HTMLInputElement>document.getElementById('correo')).value;
-        const pwrd1 = (<HTMLInputElement>document.getElementById('pwrd1')).value;
-        const pwrd2 = (<HTMLInputElement>document.getElementById('pwrd2')).value;
 
         // Validaciones aquí
-        if (!cui || !nombre || !apellido || !categoria || !dpt || !telefono || !correo || !pwrd1 || !pwrd2) {
+        if (!cui || !nombre || !apellido || !categoria || !dpt || !telefono || !correo || !isnt) {
           Swal.showValidationMessage('Por favor complete todos los campos');
           return;
         }
 
         if (cui.length !== 13 || !/^\d+$/.test(cui)) {
           Swal.showValidationMessage('El CUI debe tener 13 dígitos y ser numérico');
-          return;
-        }
-
-        if (pwrd1 !== pwrd2) {
-          Swal.showValidationMessage('Las contraseñas no coinciden');
           return;
         }
 
@@ -138,20 +139,17 @@ export class EmployeesComponent {
           dpt: dpt,
           telefono: telefono,
           correo: correo,
-          pwrd1: pwrd1,
-          pwrd2: pwrd2,
+          instalacion: isnt
         };
       },
     }).then(async (result) => {
       if (result.isConfirmed) {
         const empleadoData = result.value;
         // Aquí puedes realizar acciones con los datos del empleado ingresados
-        console.log(empleadoData);
-        this.service.addNewEmployee(empleadoData)
-        this.clients = await this.service.getEmployees()
-        this.data = this.clients.slice(this.minIndex, this.maxIndex)
+        this.service.AddMetadata(empleadoData);
       }
-      this.service.updateEmployee(result.value)
+      this.clients = await this.service.getEmployees()
+      this.data = this.clients.slice(this.minIndex, this.maxIndex)
     });
 
 
