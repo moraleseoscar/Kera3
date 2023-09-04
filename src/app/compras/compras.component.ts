7fecf860333f620c34b4227944353ee1a219edbc
diggsy <alo20172@uvg.edu.gt>
2023-09-04 13:44:42 -0600

    update:Compras
    added all the UX it's just left the insert part

--- src/app/compras/compras.component.ts
+++ src/app/compras/compras.component.ts
@@ -13,21 +13,23 @@ export class ComprasComponent {
   maxIndex:number = 5
   currentPage: number = 1
   itemsPerPage: number = 5
+  selectedProducts: { name: string, quantity: number,cod:string }[] = [];
   data:any = []
   compras:any = []
-  coso:any =[]
+  products:any =[]
+  proveedores:any =[]
   paymentStates:string[]= ['UN SOLO PAGO','A PLAZOS']
   searchQuery: string = ''
   estadoValue = '0'
-  clienteSelected = ''
+  proveedorSelected = ''
   productSelected = ''
   paymentSelected = ''
   constructor(private service: Kera3Service) {}
   async ngOnInit() {
     this.compras = await this.service.getCompras();
-    console.log(this.compras);
+    this.products = await this.service.getAllProducts();
+    this.proveedores = await this.service.getProveedores();
     this.organizeData();
-    console.log(this.data);
   }
   getDetails(buyData:any){
     let buyList = buyData.products;
@@ -69,8 +71,90 @@ export class ComprasComponent {
       }
     }
   }
+  confirmSale() {
+    // Check if clienteSelected is empty
+    if (!this.proveedorSelected) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Debes seleccionar un proveedor.'
+      });
+      return; // Don't proceed further
+    }
+
+    // Check if selectedProducts array is empty
+    if (this.selectedProducts.length === 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'No has seleccionado ningún producto.'
+      });
+      return; // Don't proceed further
+    }
+    if(this.paymentSelected == ''){
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'No has seleccionado ningún tipo de pago'
+      });
+      return; // Don't proceed further
+    }
+    // Check for invalid quantities in selectedProducts
+    const invalidProducts = this.selectedProducts.filter(product => product.quantity <= 0);
+    if (invalidProducts.length > 0) {
+      const productNames = invalidProducts.map(product => product.name).join(', ');
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: `Las siguientes productos tienen cantidades no válidas: ${productNames}`
+      });
+      return; // Don't proceed further
+    }
+    // If all checks pass, you can proceed with sending the information
+    this.sendBuyData();
+  }
+  sendBuyData() {
+    // Perform actions to send the sale data
+    // You can send the data to your server or perform other operations here
+    // this.service.addCompra(this.user_id,this.instalation, this.clienteSelected , this.paymentSelected, this.selectedProducts);
+    // Reset form
+    this.paymentSelected = '';
+    this.proveedorSelected = '';
+    this.showBuyForm = false;
+    this.selectedProducts = [];
+  }
+  cancelSale() {
+    // Reset form
+    this.paymentSelected = '';
+    this.proveedorSelected = '';
+    this.showBuyForm = false;
+    this.selectedProducts = [];
+  }
   changePanelMode(){
     this.showBuyForm = !this.showBuyForm;
+  }
+  // Method to add a product to selectedProducts array
+  addProduct() {
+    const quantityInput = document.getElementById('quantityInput') as HTMLInputElement;
+    // Find the selected product based on codigo_producto
+    const selectedProduct = this.products.find((product: { codigo_producto: any; }) => product.codigo_producto === this.productSelected);
+    const val = parseInt(quantityInput.value, 10);
+    if (this.productSelected!='' && val > 0 && selectedProduct) {
+      this.selectedProducts.push({ name: selectedProduct.nombre_producto, quantity: parseInt(quantityInput.value,10),cod:this.productSelected });
+      this.productSelected ='';
+      quantityInput.value = '';
+    }
+  }
+  removeProduct(i: number)
+  {
+    for (let index = 0; index < this.selectedProducts.length; index++) {
+      var newArray: any[] = [];
+      if(index != i)
+      {
+        newArray.push(this.selectedProducts[index]);
+      }
+      this.selectedProducts = newArray;
+    }
   }
   returnFirstPage() {
     this.currentPage = 1
