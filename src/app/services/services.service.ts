22a01ec6c593c0ef481be3afbb867f22539a3718
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-10-22 23:46:38 -0600

    modificacion

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -404,6 +404,7 @@ export class Kera3Service {
       const minutes = String(now.getMinutes()).padStart(2, '0');
       const seconds = String(now.getSeconds()).padStart(2, '0');
       const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+      
       return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
   }
 }
